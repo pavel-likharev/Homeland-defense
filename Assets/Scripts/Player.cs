95cc6bc85e602e55610a2e33acab8f40732bfa1e
Pavel Likharev <pavel7321@mail.ru>
2023-06-26 12:52:07 +0300

    Add assets, create demo-lvl and input system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+
+    Vector2 moveInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+    Rigidbody2D rigidbody;
+    
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+        
+            
+    }
+
+    void Start()
+    {
+        InitBounds();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InitBounds()
+    {
+        Camera cam = Camera.main;
+        minBounds = cam.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = cam.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Move()
+    {
+        rigidbody.velocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+    }
+}
